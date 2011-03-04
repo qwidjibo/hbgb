1ee389ee56bce7eae4bc36af21a7d01766ce37ff
Q <q@qwidjibo.org>
2011-03-04 04:45:55 -0500

    set up landing page

---
+++ landing.py
@@ -0,0 +1,7 @@
+# just loads templates/index.html
+
+import os
+from google.appengine.ext.webapp import template
+
+path = os.path.join(os.path.dirname(__file__), 'templates', 'index.html')
+print(template.render(path, { }))
