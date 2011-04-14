fd7f28daa96a89f9bca6da25e61637be6c345282
Q <q@qwidjibo.org>
2011-04-14 20:53:50 +0100

    added dates page, got first pieces working

--- register.py
+++ register.py
@@ -8,6 +8,7 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
+import campdate
 import camper
 import camp
 import healer
@@ -101,6 +102,35 @@ class PlayaInfoPage(webapp.RequestHandler):
         dietary_restrictions = self.request.get('dietary_restrictions')
         camper.put()
 
+        self.redirect('/register/dates')
+
+class DateInfoPage(webapp.RequestHandler):
+    def get(self):
+        conf = camp.current()
+        camper = db.get(self.request.cookies['_camper_key'])
+        path = os.path.join(os.path.dirname(__file__), 'templates', 'reg_dates.html')
+
+        early_dates = db.GqlQuery('SELECT * FROM CampDate WHERE early_team=TRUE ORDER BY date').fetch(100)
+        dates = db.GqlQuery('SELECT * FROM CampDate WHERE early_team=FALSE ORDER BY date').fetch(100)
+        strike_dates = db.GqlQuery('SELECT * FROM CampDate WHERE strike=TRUE ORDER BY date').fetch(100)
+
+        self.response.out.write(template.render(path, { 'camper' : camper, 
+                                                        'camp' : conf,
+                                                        'early_dates' : early_dates,
+                                                        'dates' : dates,
+                                                        'strike_dates' : strike_dates }))
+
+    def post(self):
+        camper = db.get(self.request.cookies['_camper_key'])
+
+        camper.early_team = self.request.get('early_team') == 'on'
+        camper.strike = self.request.get('strike') == 'on'
+        camper.arrival_time = self.request.get('arrival_time')
+        camper.departure_time = self.request.get('departure_time')
+        camper.arrival_date = self.request.get('arrival_date')
+        camper.departure_date = self.request.get('departure_date')
+        
+        camper.put()
         if camper.wants_to_heal:
             self.redirect('/register/healerinfo')
         else:
@@ -155,6 +185,7 @@ application = webapp.WSGIApplication(
         ('/register', LandingPage),
         ('/register/personalinfo', PersonalInfoPage),
         ('/register/playainfo', PlayaInfoPage),
+        ('/register/dates', DateInfoPage),
         ('/register/healerinfo', HealerInfoPage),
         ('/register/photoupload', PhotoUploadPage),
         ('/register/confirm', ConfirmationPage)
